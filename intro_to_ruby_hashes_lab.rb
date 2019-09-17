ea37358ffbe55a2c2ca182588d3617169fbff1ad
Sarah Laloggia <sarah.laloggia@gmail.com>
2019-09-17 22:31:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,7 @@ def monopoly_with_third_tier
   {:railroads => {
     pieces: 4, 
     rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200},
-    names: {reading_railroad: {}, pennsylvania_railroad: {}, name_key_3: "Jill", name_key_4: "Jane"}
+    names: {reading_railroad: {}, pennsylvania_railroad: {}, b_and_o_railroad: {}, name_key_4: "Jane"}
     
   }
   }
