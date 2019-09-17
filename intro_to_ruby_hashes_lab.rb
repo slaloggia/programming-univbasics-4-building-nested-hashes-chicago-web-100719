55329bd7ae0d76285051cd0b59e942b935a39980
Sarah Laloggia <sarah.laloggia@gmail.com>
2019-09-17 22:19:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,7 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   
-  {:railroads => {names: {name1: n, name2: x, name3: y, name4: z}, pieces: 4, rent_in_dollars: {key1: [], key2: [], key3: [], key4: []}}}
+  {:railroads => {names: {name1: n, name2: x, name3: y, name4: z}, pieces: 4, :rent_in_dollars => {key1: [], key2: [], key3: [], key4: []}}
 
 end
 
