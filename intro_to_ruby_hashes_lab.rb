cef424b06871e52a12a56bb9009cfc14a43dc052
Sarah Laloggia <sarah.laloggia@gmail.com>
2019-09-17 22:10:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,6 +9,7 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  
   {:railroads => {pieces: 4 }}
 
 end
@@ -18,6 +19,9 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  
+  {:railroads => {pieces: 4, rent_in_dollars: 50}}
+
 end
 
 def monopoly_with_fourth_tier
